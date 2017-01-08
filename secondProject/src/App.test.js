5c119913933ba893a11c6cde59026609f840944b
aady5566 <aady5566@gmail.com>
2017-01-08 15:25:35 +0800

    react test with enzyme

--- secondProject/src/App.test.js
+++ secondProject/src/App.test.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>,
+//    div);
+// });
+
+// it('renders without crashing', () => {
+//   shallow(
+//     <Provider store={createStoreWithMiddleware(reducers)}>
+//       <App />
+//     </Provider>
+//   );
+// });
+
+
+it('renders welcome message', () => {
+  const wrapper = shallow(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>
+  );
+  const welcome = <h2>Welcome to React</h2>;
+  console.log(welcome);
+  // expect(wrapper.contains(welcome)).to.equal(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
