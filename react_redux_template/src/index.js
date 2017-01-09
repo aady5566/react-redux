16d99d6b33a4e84433be2b9cc702e62446d2eb70
aady5566 <aady5566@gmail.com>
2017-01-09 22:56:06 +0800

    with redux devtools

--- react_redux_template/src/index.js
+++ react_redux_template/src/index.js
@@ -4,11 +4,11 @@ import App from './App';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
-
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+import { composeWithDevTools } from 'redux-devtools-extension';
+const createDevWithMiddleware = composeWithDevTools(applyMiddleware())
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStore(reducers,createDevWithMiddleware)}>
     <App />
   </Provider>,
   document.getElementById('root')
